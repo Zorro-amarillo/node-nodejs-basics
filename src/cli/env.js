e886b0563d21d1bd392eefd72a26c8e5c26691fe
Yana Malakhova <ya6ka0@gmail.com>
2025-10-24 16:29:40 +0300

    feat: complete cli subtasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,22 @@
+import { env, exit, stderr, stdout } from 'process';
+
 const parseEnv = () => {
-  // Write your code here
+  const envVars = Object.entries(env)
+    .filter(arr => arr[0].startsWith('RSS_'));
+
+  if (envVars.length === 0) {
+    stderr.write('No RSS_ environment variables found\n');
+    exit();
+  }
+
+  const rssVars = envVars.map(arr => {
+    const [ key, value ] = arr;
+      return `${key}=${value}`;
+    })
+    .join('; ');
+
+  stdout.write(rssVars + '\n');
+  exit();
 };
 
 parseEnv();
