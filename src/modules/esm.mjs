7d2fe31833e6d8bc542cfabd64ea9b7dda83609f
Yana Malakhova <ya6ka0@gmail.com>
2025-10-25 21:56:50 +0300

    feat: complete modules subtask

---
+++ src/modules/esm.mjs
@@ -0,0 +1,43 @@
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import { dirname, sep } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import './files/c.cjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  const objA = await import('./files/a.json', { with: { type: 'json' } });
+  unknownObject = objA.default;
+} else {
+  const objB = await import('./files/b.json', { with: { type: 'json' } });
+  unknownObject = objB.default;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
