604d082d930d29f42061f17cfdbd0ca05ef029a4
Yana Malakhova <ya6ka0@gmail.com>
2025-10-25 23:49:20 +0300

    feat: complete hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { dirname } from "path";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const readStream = createReadStream(`${__dirname}/files/fileToCalculateHashFor.txt`);
+  const hash = createHash('sha256');
+  readStream.pipe(hash).setEncoding('hex').pipe(stdout);
+
+  hash.on('end', () => stdout.write('\n'));
 };
 
 await calculateHash();
